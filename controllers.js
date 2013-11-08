327c4c975002d8402328a1fe46c2cf67a04ecb31
daisy1754 <d-part@hotmail.co.jp>
2013-11-08 18:13:47 +0900

    Add functionality for querying /repositries API

--- controllers.js
+++ controllers.js
@@ -6,4 +6,27 @@ App.controller('RepoListCtrl', function RepoListCtrl($scope, $http) {
       $http.get("https://api.github.com/search/repositories?q=language:javascript&sort=updated&order=desc")
           .success(function(data) {$scope.repos = data.items});
   };
+
+  $scope.queryRepositoriesAPI = function() {
+      delete $scope.repos;
+      if (!$scope.numOfRepositoriesEstimate) {
+        $http.get("https://api.github.com/search/repositories?q=created:>=" + $scope.formattedDate())
+          .success(function(data) {
+              $scope.numOfRepositoriesEstimate = data.items[0].id;
+              $scope.queryNewerRepositories();
+          });
+      } else {
+        $scope.queryNewerRepositories();
+      }
+  };
+
+  $scope.queryNewerRepositories = function() {
+      $http.get("https://api.github.com/repositories?since=" + $scope.numOfRepositoriesEstimate)
+          .success(function(data) {$scope.repos = data});
+  }
+
+  $scope.formattedDate = function() {
+      var now = new Date();
+      return now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+  }
 });
